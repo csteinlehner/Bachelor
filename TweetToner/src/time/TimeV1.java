07dd4259c904ca2f3fb74b46cc0c6d8709421223
norrix <norrix@192.168.123.103>
2012-09-27 18:24:37 +0200

    – curve-thickness shows tweet count

--- TweetToner/src/time/TimeV1.java
+++ TweetToner/src/time/TimeV1.java
@@ -54,9 +54,47 @@ public class TimeV1 extends PApplet{
 		int start = 0;
 		int end = start+24;
 		float step = 2*PI/(end-start);
-		rectMode(CENTER);
+		 
+//		fill(0,150,0);
+		
+		float tCountRadius = 150;
+		fill(200,150,150);
+		pushMatrix();
+		translate(width/2,height/2);
+		rotate(-HALF_PI);
+		beginShape();
+		curveVertex(cos(step*0) * (tCountRadius+data.get(0).t_count*0.1f),sin(step*0) * (tCountRadius+data.get(0).t_count*0.1f));
+		for (int i = start; i < end; i++) {
+			 float offset = data.get(i).t_count * 0.1f;
+			  float x = cos(step*i) * (tCountRadius+offset);
+			  float y = sin(step*i) * (tCountRadius+offset);
+			curveVertex(x,y);
+		}
+//		curveVertex(cos(step*end) * (tCountRadius+data.get(end).t_count*0.1f),sin(step*end) * (tCountRadius+data.get(end).t_count*0.1f));
+		curveVertex(cos(step*0) * (tCountRadius+data.get(0).t_count*0.1f),sin(step*0) * (tCountRadius+data.get(0).t_count*0.1f));
+		endShape();
+		popMatrix();
+		
+		fill(220,230,220);
 		noStroke();
-		float radius = 200; 
+		pushMatrix();
+		translate(width/2,height/2);
+		rotate(-HALF_PI);
+		beginShape();
+		curveVertex(cos(step*0) * (tCountRadius+data.get(0).t_count*0.1f),sin(step*0) * (tCountRadius+data.get(0).t_count*0.1f));
+		for (int i = start; i < end; i++) {
+			float offset = data.get(i).t_count * 0.1f;
+			  float x = cos(step*i) * (tCountRadius-offset);
+			  float y = sin(step*i) * (tCountRadius-offset);
+			curveVertex(x,y);
+		}
+		curveVertex(cos(step*0) * (tCountRadius+data.get(0).t_count*0.1f),sin(step*0) * (tCountRadius+data.get(0).t_count*0.1f));
+		endShape();
+		popMatrix();
+		
+		float radius = 200;
+		// foursquare cat names
+		noStroke();
 		for (int i = start; i < end; i++) {
 			SingleData dp = data.get(i);
 			pushMatrix();
@@ -65,26 +103,17 @@ public class TimeV1 extends PApplet{
 			translate(0,-radius);
 //			float sizeB = 10*sqrt(data.get(i).fsq_count);
 			fill(40,30,30);
-			rotate(-HALF_PI);
 			if(dp.hasCats){
-				
+				rotate(-HALF_PI);
 				float sizeF = sqrt(dp.categories.descendingMultiset().firstEntry().getCount())*10;
 				textSize(fontSize+sizeF);
 				text(dp.categories.descendingMultiset().firstEntry().getElement(),0,0);
 			}
 			popMatrix();
 		}
-		
-//		for (int i = start; i < end; i++) {
-//			pushMatrix();
-//			translate(width/2,height/2);
-//			rotate(step*i);
-//			translate(0,-400);
-//			float sizeA = 10*sqrt(data.get(i).t_count);
-//			fill(140,130,130);
-//			rect(0,0,sizeA,sizeA);
-//			popMatrix();
-//		}
+		// foursquare quads
+		rectMode(CENTER);
+		noStroke();
 		for (int i = start; i < end; i++) {
 			pushMatrix();
 			translate(width/2,height/2);
