e30072c7170fcc531a794e49993a418a5ae456ed
norrix <norrix@192.168.123.103>
2012-10-02 19:03:35 +0200

    – first grid by table

--- TweetToner/src/citygrid/CityGrid.java
+++ TweetToner/src/citygrid/CityGrid.java
@@ -4,11 +4,16 @@ package citygrid;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Vector;
 
 import citygrid.WeekDay;
 
 import com.csvreader.CsvReader;
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.Table;
 
 import processing.core.PApplet;
 import processing.core.PFont;
@@ -17,7 +22,7 @@ public class CityGrid extends PApplet{
 	
 	protected static final String CITY = "berlin";
 	
-	private static String csvPath = "data/fsq_timecount_days_"+CITY+".csv";
+	private static String csvPath = "data/tweetcount_matrix_60_"+CITY+".csv";
 	
 	public static PApplet p5; 
 	
@@ -25,8 +30,14 @@ public class CityGrid extends PApplet{
 	
 	private float fontSize = 12f;
 	
+	private float hourSize = 20f;
+	
 	int start = 0;
 	int end = start+24;
+	
+	// holds the tweetCount with schema day (0-6), time(0-23), t_count
+	Table<Integer, Integer, Integer> tweetCount = HashBasedTable.create();
+	Table<Integer, Integer, Integer> tweetCountAdded = HashBasedTable.create();
 	
 	Vector<WeekDay> weekdaydata = new Vector<WeekDay>();
 	Vector<DayStreet> daystreets = new Vector<DayStreet>();
@@ -39,15 +50,20 @@ public class CityGrid extends PApplet{
 		font = createFont("Axel-Bold",20);
 			try {
 			
-			CsvReader csvData = new CsvReader(csvPath,',',Charset.forName("UTF-8"));
+			CsvReader csvData = new CsvReader(csvPath,';',Charset.forName("UTF-8"));
 		
 			csvData.readHeaders();
-
+			int day = 0;
+			int time = 0;
+			int hCount = csvData.getHeaderCount();
 			while (csvData.readRecord())
 			{
-				weekdaydata.add(new WeekDay(csvData.get("time"), Integer.parseInt(csvData.get("count"))));
+				for (int i = 0; i < hCount; i++) {
+					tweetCount.put(i, time, Integer.parseInt(csvData.get(i)));
+				}
+//				tweetCount.put();
+				time++;
 			}
-	
 			csvData.close();
 			
 		} catch (FileNotFoundException e) {
@@ -55,9 +71,23 @@ public class CityGrid extends PApplet{
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-			for (int i = 0; i < 7; i++) {
-				daystreets.add(new DayStreet(i+1, weekdaydata.get(i).t_count));
+			Map<Integer, Map<Integer, Integer>> rMap = tweetCount.rowMap();
+			for (int i = 0; i < rMap.size(); i++) {
+				Map<Integer, Integer> tMap = rMap.get(i);
+				for (int j = 0; j < tMap.size(); j++) {
+					if(j>0){
+						tweetCountAdded.put(i, j, tMap.get(j)+tweetCountAdded.get(i,j-1));
+					}else{
+						tweetCountAdded.put(i, j, tMap.get(j));
+					}
+				}
 			}
+			println(tweetCount);
+			println(tweetCountAdded);
+			println(tweetCountAdded.row(0));
+//			for (int i = 0; i < 7; i++) {
+//				daystreets.add(new DayStreet(i+1, weekdaydata.get(i).t_count));
+//			}
 			
 			textFont(font);
 	}
@@ -68,22 +98,42 @@ public class CityGrid extends PApplet{
 		float step = 2*PI/(end-start);
 		
 		float weekstep = 2*PI/7;
-		
-		pushMatrix();
-	translate(100,height/4);
-//		rotate(HALF_PI);
-		stroke(0);
-		noFill();
-		for (int i = 0; i < 7; i++) {
-//			WeekDay d = weekdaydata.get(i);
-//			rotate(weekstep);
-			pushMatrix();
-			rotate(HALF_PI);
-			daystreets.get(i).draw();
-			popMatrix();
-			CityGrid.p5.translate(140,0);
+		translate(100,100);
+		for (int i = 0; i < 24; i++) {
+			beginShape();
+			vertex(0,i*hourSize);
+//			println(tweetCountAdded.get(0,1));
+//			println("--"+i);
+			for (int j = 0; j < 7; j++) {
+//				println(tweetCountAdded.get(i,j));
+				vertex(tweetCountAdded.get(i, j),i*hourSize);
+			}	
+			endShape();
 		}
-		popMatrix();
+		for (int i = 0; i < 7; i++) {
+			beginShape();
+			for (int j = 0; j < 24; j++) {
+				vertex(tweetCountAdded.get(j, i),j*hourSize);
+			}
+			endShape();
+		}
+		
+//		
+//		pushMatrix();
+//	translate(100,height/4);
+////		rotate(HALF_PI);
+//		stroke(0);
+//		noFill();
+//		for (int i = 0; i < 7; i++) {
+////			WeekDay d = weekdaydata.get(i);
+////			rotate(weekstep);
+//			pushMatrix();
+//			rotate(HALF_PI);
+//			daystreets.get(i).draw();
+//			popMatrix();
+//			CityGrid.p5.translate(140,0);
+//		}
+//		popMatrix();
 		
 //		pushMatrix();
 //		translate(width/2,height/2);
---
+++ TweetToner/src/citygrid/TimeCoordinate.java
@@ -0,0 +1,11 @@
+package citygrid;
+
+public class TimeCoordinate {
+	Integer t_count;
+	Integer day;
+	Integer time;
+	public TimeCoordinate() {
+		
+	}
+
+}
